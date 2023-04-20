ced027329b0d6cace4eacbf651431b1c81667ab8
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-04-20 17:27:52 +0200

    add: pari o dispari-

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -1,6 +1,22 @@
 const userNumber = parseInt(prompt('inserisci un numero da 1 a 5'));
 
-for (let i = 0; i < 5; i++){
+min = 1
+max = 5
+
+function numberComputer (min, max){
     
+    let  numberPcRandom  = Math.floor(Math.random() * (max - min) ) + min;
+
+    if(numberPcRandom > userNumber){
+        console.log ("Ha vinto l'utente")
+    }
+
+    else if(userNumber > numberPcRandom){
+        
+        console.log('Ha vinto il computer')
+    }
 }
 
+numberComputer (userNumber)
+
+    
