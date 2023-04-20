63c2717bfb7578568beddb9a9c0f4494a845c6c0
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-04-20 16:51:18 +0200

    add: completed

--- palidroma/script.js
+++ palidroma/script.js
@@ -1,32 +1,85 @@
-let word
-let contraryWord
+const parola = (prompt('Inserisci una parola'));
 
+function palindromo(word){
 
+    let parolaReverse = word.split('').reverse().join('');
 
-for (let i = 0; i < 1; i++ ){
-    word = (prompt('Inserisci una parola'))
-    contraryWord = (prompt('Inserisci la parola però al contrario'));
-
-     if (word == contraryWord){
-         console.log('è palindroma ')
-     }
-
-     else if (contraryWord !== word){
-         console.log ('non è palindroma')
+    if( parolaReverse == word){
+       console.log('è palindormo')
     }
-}
+
+    else{
+        console.log('non è palindromo')
+    }
+};
+
+palindromo(parola);
 
 
-// function Name (word, contraryWord){
-    
-//     if (word == contraryWord){
-//         console.log('è palindroma ')
-//     }
-
-//     else if (contraryWord !== word){
-//         console.log ('non è palindroma')
-//    }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function parolaInversa (word){
+//    for (let i = 0; i < word.length; i--){
+// }
 
 // }
+
+
+
+// function confrontoParola (word){
+    
+//     if (word == parolaInversa){
+//         return true
+//     }
+
+//     else{
+//         return false 
+//      }   
+// }
+
+
+//  let result = confrontoParola (word)
+
+//  if(result == true){
+//      console.log('è palindroma ')
+//  }
+
+
+//  else{
+//     console.log ('non è palindroma')
+// }
+
+
