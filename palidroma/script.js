007ffd90e74a153a5852e009fcd0b0958b3226d3
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-04-20 15:07:10 +0200

    .

--- palidroma/script.js
+++ palidroma/script.js
@@ -1,17 +1,32 @@
 let word
 let contraryWord
 
+
+
 for (let i = 0; i < 1; i++ ){
     word = (prompt('Inserisci una parola'))
-    contraryWord = (prompt('Inserisci la parola però al contrario'))
+    contraryWord = (prompt('Inserisci la parola però al contrario'));
 
-    if (word == contraryWord){
-        console.log('è palindroma ')
-    }
+     if (word == contraryWord){
+         console.log('è palindroma ')
+     }
 
-    else if (contraryWord !== word){
-        console.log ('non è palindroma')
+     else if (contraryWord !== word){
+         console.log ('non è palindroma')
     }
 }
 
 
+// function Name (word, contraryWord){
+    
+//     if (word == contraryWord){
+//         console.log('è palindroma ')
+//     }
+
+//     else if (contraryWord !== word){
+//         console.log ('non è palindroma')
+//    }
+
+
+
+// }
---
+++ pari e dispari/script.js
@@ -0,0 +1,6 @@
+const userNumber = parseInt(prompt('inserisci un numero da 1 a 5'));
+
+for (let i = 0; i < 5; i++){
+    
+}
+
