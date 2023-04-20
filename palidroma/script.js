fdf4875b56003e0e3fe5869729df6df565428a75
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-04-20 19:01:46 +0200

    add: correction palindromo

--- palidroma/script.js
+++ palidroma/script.js
@@ -1,20 +1,39 @@
-
-const parola = (prompt('Inserisci una parola'));
-
+// const parola = (prompt('Inserisci una parola'));
 
 
-function palindromo(word){
 
-    let parolaReverse = word.split('').reverse().join('');
+// function palindromo(word){
 
-    if( parolaReverse == word){
-       console.log('è palindormo')
+//     let parolaReverse = word.split('').reverse().join('');
+
+//     if( parolaReverse == word){
+//        console.log('è palindormo')
+//     }
+
+//     else{
+//         console.log('non è palindromo')
+//     }
+// };
+
+// palindromo(parola);
+
+ const parola = (prompt('Inserisci una parola'));
+
+ function palindromo (word){
+ 
+    let reverse = '';
+    
+    for (let i = word.length -1; i >= 0; i-- ){
+
+        reverse += word[i];
     }
 
+    if(reverse == word){
+        console.log('è palindromo');
+    }
     else{
-        console.log('non è palindromo')
+        console.log("non è palindormo");
     }
-};
+}
 
 palindromo(parola);
-
